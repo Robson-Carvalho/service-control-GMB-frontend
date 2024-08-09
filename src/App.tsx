6ce4c19eb5e22c950608a3d6c1aa77ec01fe9b7b
Robson Carvalho <robsoncarvalho.tech@gmail.com>
2024-08-08 21:11:44 -0300

    feat: adiciona sistema de navegação

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { AuthProvider } from "./context/auth";
+import PrivateRoute from "./components/PrivateRoutes";
+
+import { Home } from "./screens/Home";
+import { SignIn } from "./screens/SignIn";
+import { Dashboard } from "./screens/Dashboard";
+
 export const App = () => {
   return (
-    <>
-      <h1>Hello, world!</h1>
-    </>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<SignIn />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,7 @@
+export const SignIn = () => {
+  return (
+    <>
+      <h1>sign in</h1>
+    </>
+  );
+};
