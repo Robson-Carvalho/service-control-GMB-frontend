e10c3450eeaa56f942de259dd20c88a1fddf8144
Robson Carvalho <robsoncarvalho.tech@gmail.com>
2024-08-15 11:03:49 -0300

    feat: adiciona redirecionamento para tela de login por token inválido

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,3 +3,18 @@ import axios from "axios";
 const baseURL = import.meta.env.VITE_BASE_URL;
 
 export const Api = axios.create({ baseURL });
+
+Api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("@Auth:token");
+      localStorage.removeItem("@Auth:user");
+      window.location.reload();
+    }
+
+    return Promise.reject(error);
+  }
+);
