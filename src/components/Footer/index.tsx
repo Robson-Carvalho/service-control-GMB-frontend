6c6567e6716941f5852735cf5e961e34b255c6bb
Robson Carvalho <robsoncarvalho.tech@gmail.com>
2024-08-19 16:27:59 -0300

    feat: adiciona margem ao topo do footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="w-full text-center text-xs text-muted-foreground p-4 bg-slate-200">
+    <footer className="mt-4 w-full text-center text-xs text-muted-foreground p-4 bg-slate-200">
       &copy; {new Date().getFullYear()} Next Automações. Todos os direitos
       reservados.
     </footer>
